5e7c737a91b814d5dfc7f7f2929cc2c313a48e5c
Arturo Castañeda <arturouce@gmail.com>
2022-06-14 19:02:52 -0500

    se agrega funcionalidad para paginación y routes

--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Pagination from 'react-bootstrap/Pagination'
 import { useState } from 'react'
 
 import LibraryList from './LibraryList'
@@ -9,16 +10,19 @@ import '../../styles/alert-styles.css'
 import LibraryFormModal from './LibraryFormModal'
 import useBookHook from '../../hooks/useBooksHook'
 import AlertMessage from '../../commons/AlertMessage'
+import usePaginationHook from '../../hooks/usePaginationHook'
 
 const Library = () => {
     const [books, setBooks] = useState([])
 
     const [updatedBook, setUpdatedBook] = useState(null)
-    const [optionsAlert, setOptionsAlert] = useState({alertShow: false})
+    const [optionsAlert, setOptionsAlert] = useState({ alertShow: false })
     const [show, setShow] = useState(false)
 
     const { handleSaveBook, handleDeleteBook, handleUpdateBook, selectUpdateBook } =
         useBookHook(setBooks, books, setUpdatedBook, setShow, setOptionsAlert)
+
+    const {items, paginatedBooks} = usePaginationHook(books)
 
     return (
         <>
@@ -31,7 +35,7 @@ const Library = () => {
                 <Row>
 
                     <Col>
-                        <LibraryFormModal 
+                        <LibraryFormModal
                             setOptionsAlert={setOptionsAlert}
                             show={show}
                             setShow={setShow}
@@ -44,13 +48,14 @@ const Library = () => {
                 <br></br>
                 <Row>
                     <Col><LibraryList
-                        books={books}
+                        books={paginatedBooks}
                         setShow={setShow}
                         selectUpdateBook={selectUpdateBook}
                         setBooks={setBooks}
                         handleDeleteBook={handleDeleteBook}
                     /></Col>
                 </Row>
+                <Pagination size="sm">{items}</Pagination>
             </Container>
         </>
     )
